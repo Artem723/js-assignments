7488898ad7f49d32bb6d01e5773583c537c22df4
Artyom Savchuk <artemsavchuk01@gmail.com>
2016-03-27 16:14:08 +0300

    Completed tasks strings

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 /********************************************************************************************
  *                                                                                          *
  * Plese read the following tutorial before implementing tasks:                             *
@@ -22,7 +23,7 @@
  *   '',  'bb'  => 'bb'
  */
 function concatenateStrings(value1, value2) {
-    throw new Error('Not implemented');
+    return value1 + value2;
 }
 
 
@@ -38,7 +39,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-    throw new Error('Not implemented');
+    return value.length;
 }
 
 /**
@@ -55,7 +56,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+        return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -69,7 +70,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+
+    return value.substring( value.indexOf(', ')+2 , value.length-1 );
+
 }
 
 
@@ -84,7 +87,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value.charAt(0);
 }
 
 /**
@@ -99,7 +102,7 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-    throw new Error('Not implemented');
+    return value.trim();
 }
 
 /**
@@ -114,12 +117,12 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'cat', 3 => 'catcatcat'
  */
 function repeatString(value, count) {
-    throw new Error('Not implemented');
+    return value.repeat(count);
 }
 
 /**
  * Remove the first occurrence of string inside another string
- * 
+ *
  * @param {string} str
  * @param {string} value
  * @return {string}
@@ -130,7 +133,10 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-    throw new Error('Not implemented');
+    var startIndex = str.indexOf(value);
+
+        return str.substring(0, startIndex) + str.substring( startIndex + value.length);
+
 }
 
 /**
@@ -145,7 +151,7 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-    throw new Error('Not implemented');
+    return str.substring(1, str.length-1);
 }
 
 
@@ -160,7 +166,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-    throw new Error('Not implemented');
+    return str.toUpperCase();
 }
 
 /**
@@ -174,7 +180,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    return str.split(';');
 }
 
 /**
@@ -201,7 +207,15 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-    throw new Error('Not implemented');
+    var firstLine;
+    var Lines;
+    var lastLine;
+
+    firstLine =  "┌" + "─".repeat(width - 2) + "┐\n";
+    Lines =     ("│" + " ".repeat(width - 2) + "│\n").repeat(height - 2);
+    lastLine =   "└" + "─".repeat(width - 2) + "┘\n";
+    return firstLine + Lines + lastLine;
+
 }
 
 
@@ -221,7 +235,28 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-    throw new Error('Not implemented');
+    var i;
+    var ch;
+    var result="";
+    var CODE_A = 'A'.charCodeAt(0);
+    var CODE_Z = 'Z'.charCodeAt(0);
+    var CODE_a = 'a'.charCodeAt(0);
+    var CODE_z = 'z'.charCodeAt(0);
+    for(i=0;i<str.length;i++)
+    {
+        ch=str.charCodeAt(i);
+        if(ch >=CODE_A && ch <=CODE_Z )
+        {
+            ch = ch + 13;
+            if(ch>CODE_Z)ch-=26;
+        }else if(ch >=CODE_a && ch <=CODE_z )
+        {
+            ch = ch + 13;
+            if(ch>CODE_z)ch-=26;
+        }
+        result += String.fromCharCode(ch);
+    }
+    return result;
 }
 
 /**
@@ -238,36 +273,55 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    if(value === undefined || value === null)return false;
+    else if(typeof value.valueOf() === "string")return true;
+    return false;
+
+
+
 }
 
 
 /**
  * Returns playid card id.
- * 
+ *
  * Playing cards inittial deck inclides the cards in the following order:
- * 
+ *
  *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
  *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
  *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
  *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
- * 
+ *
  * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
  * Function returns the zero-based index of specified card in the initial deck above.
- * 
+ *
  * @param {string} value
  * @return {number}
  *
  * @example
  *   'A♣' => 0
- *   '2♣' => 1 
+ *   '2♣' => 1
  *   '3♣' => 2
  *     ...
  *   'Q♠' => 50
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+
+    var cards = "'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣'," +
+                "'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦'," +
+                "'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥'," +
+                "'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'" ;
+
+    cards = cards.substring(1,cards.length-1);
+    cards = cards.replace(/','/g, "");
+    cards = cards.replace(/10/g, "1");
+
+    value =  value.replace(/10/, "1");
+
+    return cards.indexOf(value) / 2;
+
+
 }
 
 
